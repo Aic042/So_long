b3bb06efb3aae404448e799dd48d053f6f2e88f6
Aic042 <159058707+Aic042@users.noreply.github.com>
2024-12-02 08:20:00 +0100

    Create mapchecker.c

---
+++ mapchecker.c
@@ -0,0 +1,7 @@
+
+
+//a function checking through gnl_function if the map.ber 
+//a strchr fucntion checking what the gnl function gets
+//if (i != 0, P, E, W)
+//perror "map couldn't be loaded"
+//return
