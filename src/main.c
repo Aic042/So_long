eaa3a54506ad48a9185f709f190903a3c7417e83
root <root@DESKTOP-7CAO994.>
2025-03-13 06:42:47 +0100

    2025-03-13 06:42:47

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aingunza <aingunza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:27:37 by aingunza          #+#    #+#             */
-/*   Updated: 2025/03/10 16:20:19 by aingunza         ###   ########.fr       */
+/*   Updated: 2025/03/12 22:55:18 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,8 @@ void	null_settlers(t_game *game)
 
 int	dealbreakers(t_game *game)
 {
+	if (!are_row_lengths_consistent(game->map->map2d, game->map->rows))
+        return (ft_printf("Error: Map rows have inconsistent lengths.\n"), ft_end_game(game), 1);
 	if (wall_checker(game) != 0 || ft_file_validator_map(game) != 0)
 		return (ft_end_game(game), 1);
 	if (init_game(game) != 0)
